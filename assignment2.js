896ee6c66e9b82fbf15aee518f29cd77960869bf
Phoebe <ruthowy@gmail.com>
2026-05-01 08:58:34 -1100

    I did and completed my 3 pending assignments, scripteded them to index.html and I'm done

--- assignment2.js
+++ assignment2.js
@@ -12,10 +12,10 @@ let johnBMI = johnMass / johnHeight ** 2;
 // 3. Create a boolean variable 'markHigherBMI' containing information about whether Mark has a higher BMI than John.
 let markHigherBMI = markBMI > johnBMI;
 
-console.log(markBMI);
+// console.log(markBMI);
 
-console.log(johnBMI);
-console.log(markHigherBMI);
+// console.log(johnBMI);
+// console.log(markHigherBMI);
 
 // TEST DATA 2
 // 1. Store Mark's and John's mass and height in variables
@@ -31,28 +31,28 @@ let johnBMI2 = johnMass2 / johnHeight2 ** 2;
 // 3. Create a boolean variable 'markHigherBMI' containing information about whether Mark has a higher BMI than John.
 let markHigherBMI2 = markBMI2 > johnBMI2;
 
-console.log(markBMI2);
-console.log(johnBMI2);
-console.log(markHigherBMI2);
+// console.log(markBMI2);
+// console.log(johnBMI2);
+// console.log(markHigherBMI2);
 
 // CODING CHALLENGE 2
 // 1.Print a nice output to the console, saying who has the higher BMI.
-console.log(`Mark's BMI is higher than John's!`);
+//console.log(`Mark's BMI is higher than John's!`);
 // Use a template literal to include the BMI values in the outputs
-console.log(`Mark's BMI (${markBMI}) is higher than John's BMI (${johnBMI})`);
+// console.log(`Mark's BMI (${markBMI}) is higher than John's BMI (${johnBMI})`);
 
 // Using if/else statement
 // TEST DATA 1
 if (markBMI > johnBMI) {
-console.log(`Mark's BMI (${markBMI}) is higher than John's (${johnBMI})!`);
+//console.log(`Mark's BMI (${markBMI}) is higher than John's (${johnBMI})!`);
   } else {
-   console.log(`John's BMI (${johnBMI2}) is higher than Mark's (${markBMI2})!`);
+  // console.log(`John's BMI (${johnBMI2}) is higher than Mark's (${markBMI2})!`);
 }
   // TEST DATA 2
 if (markBMI2 > johnBMI2) {
-    console.log(`Mark's BMI (${markBMI2}) is higher than John's (${johnBMI2})!`);
+    //console.log(`Mark's BMI (${markBMI2}) is higher than John's (${johnBMI2})!`);
 } else {
-    console.log(`John's BMI (${johnBMI2}) is higher than Mark's (${markBMI2})!`);
+    //console.log(`John's BMI (${johnBMI2}) is higher than Mark's (${markBMI2})!`);
 }
 
 
